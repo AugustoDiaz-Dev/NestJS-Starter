5229fbcebcf43e8e853f15bbec20b4c3326dcdd1
Augusto <augustodiaz_1@hotmail.com>
2022-07-18 10:27:11 +1000

    Final version

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Get, Param, Patch, Delete, Body } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
@@ -11,7 +11,39 @@ export class ProductsController {
         @Body('description') prodDesc: string,
         @Body('price') prodPrice: number
     ) {
-        const generatedId = this.productsService.insertProduct(prodTitle, prodDesc, prodPrice);
+        const generatedId = this.productsService.insertProduct(
+            prodTitle,
+            prodDesc,
+            prodPrice
+        );
         return { id: generatedId }
     }
+
+    @Get()
+    getAllProducts() {
+        return this.productsService.getProducts();
+    }
+
+    @Get('/:id')
+    getProduct(@Param('id') prodId: string) {
+        return this.productsService.getSingleProduct(prodId);
+    }
+
+    @Patch('/:id')
+    updateProduct(
+        @Param('id') prodId: string,
+        @Body('title') prodTitle: string,
+        @Body('description') prodDesc: string,
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        @Body('price') prodPrice: number) {
+        this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+        return null;
+    }
+
+    @Delete('/:id')
+    removeProduct(@Param('id') prodId: string) {
+        this.productsService.deleteProduct(prodId);
+        return null;
+    }
+
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from './product.model';
 
 @Injectable()
@@ -6,7 +6,7 @@ export class ProductsService {
     products: Product[] = [];
 
     insertProduct(title: string, description: string, price: number): string {
-        const prodId = new Date().toString();
+        const prodId = Math.random().toString();
         const newProduct = new Product(
             prodId,
             title,
@@ -15,4 +15,48 @@ export class ProductsService {
         this.products.push(newProduct);
         return prodId;
     }
+
+    getProducts() {
+        return [... this.products];
+    }
+    getSingleProduct(productId: string) {
+        const product = this.findProduct(productId)[0];
+        return { ...product };
+    }
+
+    updateProduct(
+        productId: string,
+        title: string,
+        desc: string,
+        price: number
+    ) {
+        const [product, index] = this.findProduct(productId);
+        const updatedProduct = { ...product }
+        if (title) {
+            updatedProduct.title = title;
+        }
+        if (desc) {
+            updatedProduct.description = desc;
+        }
+        if (price) {
+            updatedProduct.price = price;
+        }
+        this.products[index] = updatedProduct;
+    }
+
+    deleteProduct(productId: string) {
+        const index = this.findProduct(productId)[1];
+        this.products.splice(index, 1);
+    }
+
+
+    private findProduct(id: string): [Product, number] {
+        const productIndex = this.products.findIndex(prod => prod.id === id);
+        const product = this.products[productIndex]
+        if (!product) {
+            throw new NotFoundException('Could not find product.');
+        }
+        return [product, productIndex];
+    }
+
 }
